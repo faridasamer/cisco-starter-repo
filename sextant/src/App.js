b7e980d08284098669d8b4dbb3873cb6caaeb608
faridasamer <faridasamer2@gmail.com>
2022-08-09 19:50:05 +0200

    Card component fetches IP

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,38 +1,52 @@
 import './App.css';
-import * as React from 'react';
+import React, { Component, useState } from 'react';
 
 function App() {
   return (
     <>
-    <Banner /> 
-    <Card/>
+    <Banner />
+    <Card name="IPv4" children='https://api.ipify.org'></Card>
+    <Card name="IPv6" children='https://api64.ipify.org'></Card>
     </>
   );
 }
 
-function Banner(){
-  return(
-    <div class="container banner">
-      <div class="jumbotron">
-      <h1>Sextant</h1>      
-      <p>Display your public IP address</p>
-      </div>    
-    </div>
-  );
-    
+class Banner extends Component {
+  render() {
+    return(
+      <div class="container banner">
+        <div class="jumbotron">
+        <h1>Sextant</h1>      
+        <p>Display your public IP address</p>
+        </div>    
+      </div>
+    );
+  }
 }
 
-function Card(){
-  return(
-    <div class="cardContainer">
-    <div class="card">
-      <div class="card-body">
-      <h5 class="card-title">Your IP address is:</h5>
-      <p class="card-text">100.100.100.100</p>
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      details: null
+    };
+  }
+  render() {
+      fetch(this.props.children)
+      .then(response => response.text())
+      .then(data => this.setState({details: data}))
+    return(
+      <div class="cardContainer">
+      <div class="card">
+        <div class="card-body">
+        <h5 class="card-title">{this.props.name}</h5>
+        <p class="card-text">IP: {`${this.state.details}`}</p>
+        </div>
       </div>
-    </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
+
 
 export default App;
